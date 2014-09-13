b8ffc2b59af283f848398d7617b7e55e420d532b
DanielSherlock <d.t.sherlock@live.co.uk>
2014-09-13 16:04:11 +0100

    made post tile blue backed and edited link behavour

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,15 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    var links = document.links;
+    for (var i = 0; i < links.length; i++) {
+        if (links[i].hostname != window.location.hostname) {
+            links[i].target = '_blank';
+        }
+    }
+});
+
 function expand(elementId) {
-    "use strict";
     var content = document.getElementById(elementId);
     var status = document.getElementById(elementId + "status");
     if (content.className !== "expand") {
